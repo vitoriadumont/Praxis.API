7f10388029ad1fda03fd0d489fdd3304a2f176e4
Vitoria Dumont <vitria.dumont@gmail.com>
2026-08-31 11:40:11 -0300

    Feat: adiciona seed de usuário de teste para login

--- Program.cs
+++ Program.cs
@@ -2,9 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using Praxis.API.Data;
 using Praxis.API.Repositories;
 using Praxis.API.Services;
+using Microsoft.AspNetCore.Identity;
+using Praxis.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 
@@ -18,32 +19,48 @@ builder.Services.AddDbContext<AppDbContext>(
             builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
-
 builder.Services.AddScoped<IAlunoService, AlunoService>();
-
 builder.Services.AddScoped<IProfessorRepository, ProfessorRepository>();
-
 builder.Services.AddScoped<IProfessorService, ProfessorService>();
-
 builder.Services.AddScoped<IDisciplinaRepository, DisciplinaRepository>();
-
 builder.Services.AddScoped<IDisciplinaService, DisciplinaService>();
-
 builder.Services.AddScoped<IRegistroAcademicoRepository, RegistroAcademicoRepository>();
-
 builder.Services.AddScoped<IRegistroAcademicoService, RegistroAcademicoService>();
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirFront", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") 
+        policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (!context.Usuarios.Any())
+    {
+        var hasher = new PasswordHasher<Usuario>();
+
+        var usuario = new Usuario
+        {
+            Nome = "Coordenador",
+            Email = "coordenador@praxis.edu.br",
+            SenhaHash = string.Empty
+        };
+
+        usuario.SenhaHash = hasher.HashPassword(usuario, "123456");
+
+        context.Usuarios.Add(usuario);
+
+        context.SaveChanges();
+    }
+}
 
 app.UseCors("PermitirFront");
 
